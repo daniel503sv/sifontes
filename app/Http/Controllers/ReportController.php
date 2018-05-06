f909cfa4268eb7984fd0870f29c5eb34d268887a
Jonathan <jonathan.president05@gmail.com>
2018-05-06 02:09:55 -0600

    add more logic for report and change ui

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,8 +8,24 @@ class ReportController extends Controller
 {
     public function show()
     {
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadView('welcome');
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadView('test');
+        $pdf->setPaper('a4', 'landscape');
         return $pdf->download( bcrypt(time()).'.pdf');
-    }    
+    }
+
+    public function index(){
+        $reports = \App\Prestamo::get();
+        $data = array();
+        foreach( $reports as $report){
+            $currentData["user_id"]=$report->usuario_id??"test";
+            $currentData["date"]=$report->fecha_prestamo;
+            $currentData["start_time"]=$report->fecha_prestamo;
+            $currentData["end_time"]=$report->fecha_devolucion;
+            $currentData["resource_id"]=$report->equipo_id;
+            array_push($data, $currentData);
+        }
+        
+        return view('report')->with("data",$data);
+    }
 }
---
+++ resources/views/report.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Report</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style type="text/css" media="all">
+            table {
+                border-collapse: collapse;
+                border-spacing: 0;
+            }
+            th {
+                border: 1px solid;
+                width: 120px;
+                padding-bottom: 30px;
+            }
+            .header{
+                text-align: justify;
+            }
+            .answer{
+                text-decoration-line: underline;
+            }
+            td{
+                border: 1px solid;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td colspan="8" class="header">
+                    <div>
+                        <label>NOMBRE DE LA INSTITUCION EDUCATIVA </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                    </div>
+                    <div>
+                        <label>NOMBRE CAVERT </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                        <label>CÓDIGO INFRAESTRUCTURA </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                    </div>
+                    <div>
+                        <label>MUNICIPIO </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                        <label>DEPARTAMENTO </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                        <label>DISTRITO EDUCATIVO N° </label>
+                        <label class="answer">{{ "HOLA" }}</label>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <th>NOMBRE DEL USUARIO</th>
+                <th>FECHA DE RESERVACIÓN</th>
+                <th>HORA INICIO</th>
+                <th>HORA FIN</th>
+                <th>RECURSOS A UTILIZAR</th>
+                <th>CÓDIGO (INVENTARIO)</th>
+                <th>GRADO</th>
+                <th>SECCIÓN</th>
+            </tr>
+            
+            @foreach($data as $info)
+                <tr>
+                    <td>{{$info['user_id']}}</td>
+                    <td>{{$info['date']}}</td>
+                    <td>{{$info['start_time']}}</td>
+                    <td>{{$info['end_time']}}</td>
+                    <td>Lempita</td>
+                    <td>{{$info['resource_id']}}</td>
+                    <td>septimo</td>
+                    <td>a</td>
+                </tr>
+            @endforeach
+        <table>
+    </body>
+</html>
